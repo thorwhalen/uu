cd4f8dc2e0e69ba9d30a888b2041599e99adef70
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2025-01-11 13:23:16 +0100

    chores

--- ij/README.md
+++ ij/README.md
@@ -1,6 +1,50 @@
-
 # ij
+
 Text feature extraction
 
-
 To install:	```pip install ij```
+
+
+# Examples
+
+## TreeTokenizer
+
+A tokenizer that is the result of multiple different tokenizers that might either all be applied to the
+same text, or are used recursively to break up the text into finner pieces.
+In a deep_tokenizer tokenizers[0] tokenizes the text, then the next tokenizer, tokeizers[1] is applied to these
+tokens, and so forth. By default, the union of the tokens are returned. If token_prefixes is specified (usually,
+a different one for each tokenizer), they are prepended to the tokens to distinguish what level of tokenization
+they come from.
+
+If text_collection is specified, along with max_df and/or min_df, the text_collection will serve to learn a
+vocabulary for each tokenizer by collecting only those tokens whose frequency is at least min_df and no more than
+max_df.
+
+Params:
+    * tokenizers: A list of tokenizers (function taking text and outputing a list of strings
+    * token_prefixes: A list of prefixes to add in front of the tokens matched for each tokenizer
+    (or a single string that will be used for all tokenizers
+    * max_df and min_df: Only relevant when leaning text_collection is specified.
+    These are respectively the max and min frequency that tokens should have to be included.
+    The frequency can be expressed as a count, or a ratio of the total count.
+    Note that in the case of max_df, it will always be relative to the total count of tokens at the current level.
+    * return_tokenizer_info: Boolean (default False) indicating whether to return the tokenizer_info_list as well
+
+Fit input X is a collection of the text to learn the vocabulary with
+
+
+```python
+>>> from ij import TreeTokenizer
+>>> import re
+>>>
+>>> t = [re.compile('[\w-]+').findall, re.compile('\w+').findall]
+>>> p = ['level_1=', 'level_2=']
+>>> ttok = TreeTokenizer(tokenizers=t, token_prefixes=p)
+>>> ttok.tokenize('A-B C B')
+['level_1=A-B', 'level_1=C', 'level_1=B', 'level_2=A', 'level_2=B', 'level_2=C', 'level_2=B']
+>>> s = ['A-B-C A-B A B', 'A-B C B']
+>>> ttok = TreeTokenizer(tokenizers=t, token_prefixes=p, min_df=2)
+>>> _ = ttok.fit(text_collection=s)
+>>> ttok.transform(['A-B C B']).tolist()
+[['level_1=A-B', 'level_1=B', 'level_2=C']]
+```
--- jo/jo/__init__.py
+++ jo/jo/__init__.py
@@ -1,14 +1,17 @@
-"""Automatically making flask webservices from python objects"""
+"""
+
+Automatically making flask webservices from python objects
+
+"""
+
 from numpy import ndarray
 from pandas import Series, DataFrame
 import re
 import json
 
-from ut.others.lru_cache import lru_cache
+from functools import lru_cache
 
 # from werkzeug.exceptions import InternalServerError
-
-import ut.wserv.errors as err
 
 # args_info = defaultdict(lambda: {'type': str, 'default': None})
 # args_info['host'] = {'type': str, 'default': DEFAULT_SDACC_HOST}
@@ -243,7 +246,7 @@ class ObjWrapper(object):
 
         # get the leaf object
         if attr is None:
-            raise err.MissingAttribute()
+            raise MissingAttribute()
         obj = get_attr_recursively(
             obj, attr
         )  # at this point obj is the nested attribute object
@@ -280,11 +283,11 @@ class ObjWrapper(object):
 
         attr = kwargs.pop('attr', None)
         if attr is None:
-            raise err.MissingAttribute()
+            raise MissingAttribute()
         elif not self._is_permissible_attr(attr):
             print(attr)
             print(str(self.permissible_attr_pattern.pattern))
-            raise err.ForbiddenAttribute(attr)
+            raise ForbiddenAttribute(attr)
         if self.debug > 0:
             print(
                 (
@@ -312,73 +315,50 @@ def obj_str_from_obj(obj):
         return 'obj'
 
 
-# Will delete another day. Just spent two hours making this beautiful and useful object, and then realized
-# it had a name, and code for it (is even integrated in the standard libraries of Python 3+
-# It's called lru_cache.
-#
-# class CachedObjAccess(object):
-#     """
-#     This class allows you to not have to re-construct objects every time you need them.
-#      It works by caching the objects you've asked for before, and giving you the already constructed objects from
-#      the cache itself.
-#     It's basically a constructor memoizer that (1) has a limited cache and (2) gets rid of the cache entry that
-#     was accessed the longest time ago if the cache is at capacity when a new object needs to be inserted.
-#
-#     https://en.wikipedia.org/wiki/Cache_replacement_policies#Least_Recently_Used_.28LRU.29
-#
-#     """
-#     def __init__(self,
-#                  obj_constructor,
-#                  constructor_kwargs_for_key,
-#                  cache_size = 10,
-#                  ckey_from_key=lambda x: x):
-#         """
-#
-#         :param obj_constructor: The function (or class constructor) that constructs the objects.
-#         :param constructor_kwargs_for_key: A function that
-#         :param cache_size:
-#         :param ckey_from_key:
-#         """
-#         self.obj_constructor = obj_constructor
-#         self.constructor_kwargs_for_key = constructor_kwargs_for_key
-#         self.cache_size = cache_size
-#         self.ckey_from_key = ckey_from_key
-#         self.cobj_for_ckey = dict()  # the cache!
-#
-#     def get_obj(self, key):
-#         """
-#         Get the obj for that key, if it's in the cache (cobj_for_ckey), and if it's not,
-#         construct it (with obj_constructor(**constructor_kwargs_for_key(key)) and insert it in the cache,
-#         removing the oldest (accessed the longest time ago) entry if the cache is at capacity
-#         (i.e. len(cobj_for_ckey) >= cache_size
-#         :param key:
-#         :return:
-#         """
-#         ckey = self.ckey_from_key(key)
-#         cobj = self.cobj_for_ckey.get(ckey, None)
-#         if cobj is not None:
-#             cobj['last_accessed_utc'] = datetime.utcnow()
-#             return cobj['obj']
-#         else:
-#             cobj = {'obj': self.obj_constructor(**self.constructor_kwargs_for_key(key)),
-#                     'last_accessed_utc': datetime.utcnow()}
-#             self.insert_cobj(ckey, cobj)
-#             return cobj['obj']
-#
-#     def insert_cobj(self, ckey, cobj):
-#         if len(self.cobj_for_ckey) >= self.cache_size:  # if we're at the limit, make some space...
-#             # find the ckey with the minimum last_accessed_utc (the ckey that's been accessed the longest time ago)
-#             min_last_accessed_utc = cobj['last_accessed_utc']
-#             pop_ckey = None
-#             for _ckey, _cobj in self.cobj_for_ckey.iteritems():
-#                 if _cobj['last_accessed_utc'] < min_last_accessed_utc:
-#                     min_last_accessed_utc = _cobj['last_accessed_utc']
-#                     pop_ckey = _ckey
-#
-#             # pop the ckey that's been accessed the longest time ago
-#             if pop_ckey is not None:
-#                 self.cobj_for_ckey.pop(pop_ckey)
-#
-#         # add the {ckey: cobj} entry
-#         self.cobj_for_ckey[ckey] = cobj
-#
+class ClientError(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+
+class EmptyResponse(ClientError):
+    status_code = 400
+
+    def __init__(self, message, payload=None):
+        super(EmptyResponse, self).__init__(message, self.status_code, payload)
+
+
+class BadRequest(ClientError):
+    status_code = 400
+
+    def __init__(self, message, payload=None):
+        super(BadRequest, self).__init__(message, self.status_code, payload)
+
+
+class Forbidden(ClientError):
+    status_code = 403
+
+    def __init__(self, message, payload=None):
+        super(Forbidden, self).__init__(message, self.status_code, payload)
+
+
+class ForbiddenAttribute(Forbidden):
+    def __init__(self, attr, payload=None):
+        super(self.__class__, self).__init__('Forbidden attribute: ' + attr, payload)
+
+
+class MissingAttribute(BadRequest):
+    def __init__(
+        self, message='No attribute (method or property) was specified.', payload=None
+    ):
+        super(self.__class__, self).__init__(message, payload)
